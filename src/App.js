c0cfd2403b6f43968d1ccd2359f0e4935acca076
Giwan Persaud <giwan.persaud@gmail.com>
2018-09-10 21:37:15 +0200

    loads component later

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Loadable from "react-loadable";
+
+const AsyncLoader = Loadable({
+  loader: () => import("./components/AsyncComponent/AsyncComponent"),
+  loading: () => <div>Loading...</div>
+});
 
 class App extends Component {
-  state = {
-    value: "tst"
-  }
   render() {
     return (
       <div className="App">
         <h1>This H1 is rendered on the server!</h1>
-        <h2 onClick={() => this.setState({ value: "bla" })}>{this.state.value}</h2>
+        <AsyncLoader />
       </div>
     );
   }
---
+++ src/components/AsyncComponent/AsyncComponent.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const AsyncComponent = () => <h1>This is an async component</h1>;
+export default AsyncComponent;
